b83fb23eb96776f9e9a042d0a2dfb78094c357f0
Teo <cfa@cfa.net.ru>
2024-12-01 13:21:57 +0500

    day 1

---
+++ day01.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+l1 = []
+l2 = []
+with open("day01.txt", "rt") as fl:
+    for line in fl.readlines():
+        left, right = line.strip().split()
+        l1.append(int(left))
+        l2.append(int(right))
+
+l1.sort()
+l2.sort()
+
+d = sum([abs(l-r) for (l, r) in zip(l1, l2)])
+print(d)
+
+
+cnt = Counter(l2)
+d = sum([l*cnt[l] for (l, r) in zip(l1, l2)])
+print(d)
