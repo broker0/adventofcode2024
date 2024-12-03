068a715c91219215afa5c970c9cbfac86fcd0bc7
broker0 <brok79@internet.ru>
2024-12-03 12:01:30 +0500

    small improvements

--- day03.py
+++ day03.py
@@ -2,22 +2,23 @@ import re
 
 with open("day03.txt", "rt") as fl:
     data = fl.read()
-    re = re.compile(r"(mul\((\d+),(\d+)\))|(do\(\))|(don\'t\(\))", re.MULTILINE)
+    re = re.compile(r"mul\((\d+),(\d+)\)|do\(\)|don\'t\(\)", re.MULTILINE)
     ops = re.findall(data)
 
-    enable = True
+    enabled = True
     sum1 = 0
     sum2 = 0
 
-    for op in ops:
-        if op[0]:
-            sum1 += int(op[1]) * int(op[2])
-            if enable:
-                sum2 += int(op[1]) * int(op[2])
-        elif op[3]:
-            enable = True
-        elif op[4]:
-            enable = False
+    for match in re.finditer(data):
+        op = match.group()
+        if op.startswith("mul"):
+            x, y = map(int, match.groups())
+            sum1 += x * y
+            sum2 += (x * y) * enabled
+        elif op.startswith("do()"):
+            enabled = True
+        elif op.startswith("don't()"):
+            enabled = False
         else:
             raise ValueError
 
